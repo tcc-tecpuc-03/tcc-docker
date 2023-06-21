375306b609de24849553461275dfd5dcaabefec7
Jonas Nogueira Neto <44125908+jonasnetopng@users.noreply.github.com>
2023-06-21 20:18:16 -0300

    :sparkles: feat: Dashboard

--- giggle/src/pages/Home/index.tsx
+++ giggle/src/pages/Home/index.tsx
@@ -1,98 +1,52 @@
-import { Link } from "react-router-dom";
-import { FiMenu, FiUser, FiHome, FiPlus } from "react-icons/fi";
-import { createElement, useState } from "react";
-import { useMediaQuery } from 'react-responsive';
-
-export function Sidebar() {
-  const [open, setOpen] = useState(false);
-  const isMobile = useMediaQuery({ maxWidth: 767 }); // Define a largura máxima para telas de celular
-
-  const handleOpen = () => {
-    setOpen(!open);
+import { Sidebar } from "../../components/Layout/Sidebar";
+import Chart from "react-apexcharts";
+import Card from "./components/Card";
+import "apexcharts/dist/apexcharts.css";
+import {FiShoppingBag} from 'react-icons/fi'
+export default function Home() {
+  const chartOptions = {
+    chart: {
+      id: "bar-chart",
+    },
+    xaxis: {
+      categories: ["Categoria 1", "Categoria 2", "Categoria 3", "Categoria 4", "Categoria 5"],
+    },
   };
 
-  const pages = [
+  const chartData = [
     {
-      name: "Home",
-      icon: FiHome,
-      path: "/",
+      name: "Série 1",
+      data: [30, 40, 45, 50, 49],
     },
     {
-      name: "Criar novo item",
-      icon: FiPlus,
-      path: "/createitem",
-    }
+      name: "Série 2",
+      data: [10, 15, 20, 25, 30],
+    },
   ];
 
-  if (isMobile) {
-    return (
-      <div className="flex flex-col h-screen font-titillium bg-gray-800 text-white">
-        <div className="flex items-center justify-between h-16 bg-gray-800">
-          <div className="flex items-center gap-2">
-            <button
-              onClick={() => handleOpen()}
-              className="hover:bg-gray-900 p-2 rounded"
-            >
-              <FiMenu size={24} />
-            </button>
-            <span className="text-lg">BUYSKET</span>
-          </div>
-          <button className="hover:bg-gray-900 p-2 rounded">
-            <FiUser size={24} />
-          </button>
-        </div>
-        {open && (
-          <div className="bg-gray-900">
-            <div className="flex flex-col gap-5">
-              {pages.map((page) => (
-                <Link
-                  className="hover:bg-gray-800 p-2 rounded"
-                  to={page.path}
-                  key={page.path}
-                >
-                  <div className="flex items-center gap-3 pl-6">
-                    {createElement(page.icon, { size: 20 })}
-                    <span className="text-base">{page.name}</span>
-                  </div>
-                </Link>
-              ))}
-            </div>
-          </div>
-        )}
-      </div>
-    );
-  }
-
   return (
-    <div className={`flex flex-col h-screen font-titillium bg-gray-800 text-white ${open ? "w-[78px]" : "w-[250px]"}`}>
-      <div className={`flex items-center justify-center gap-2 h-16`}>
-        {!open && <span className="text-lg">BUYSKET</span>}
-        <button
-          onClick={() => handleOpen()}
-          className="hover:bg-gray-900 p-2 rounded"
-        >
-          <FiMenu size={24} />
-        </button>
-      </div>
-      <div className={`flex items-center justify-center flex-col gap-5`}>
-        {pages.map((page) => (
-          <Link
-            className="hover:bg-gray-900 p-2 rounded"
-            to={page.path}
-            key={page.path}
+    <div className="flex">
+      <Sidebar />
+      <div className="flex flex-col w-full">
+        <div className="grid grid-cols-3 gap-4 p-4">
+          <Card title="Card 1" />
+          <Card title="Card 2" />
+          <Card title="Cestas finalizadas" 
+          icon={FiShoppingBag}
           >
-            <div className="flex flex-row gap-3">
-              {createElement(page.icon, { size: 24 })}
-              {!open && <span className="text-lg">{page.name}</span>}
-            </div>
-          </Link>
-        ))}
-      </div>
-      <div className="flex-grow">{/* Icons */}</div>
-      <div className="flex items-center justify-center h-16">
-        <button className="hover:bg-gray-900 p-2 rounded">
-          <FiUser size={24} />
-        </button>
+            <p className="text-3xl font-inter">10</p>
+
+            </Card>
+        </div>
+        <div className="p-4">
+          <Chart
+            options={chartOptions}
+            series={chartData}
+            type="bar"
+            height={300}
+            className="w-full"
+          />
+        </div>
       </div>
     </div>
   );
