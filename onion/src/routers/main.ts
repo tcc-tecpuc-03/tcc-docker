41ed326ef8ab2b9faaa075557a034832ec09c24d
Jonas Nogueira Neto <44125908+jonasnetopng@users.noreply.github.com>
2023-06-29 20:38:04 -0300

    :bug: fix: rota put remocao

--- onion/src/routers/main.ts
+++ onion/src/routers/main.ts
@@ -1,7 +1,7 @@
-import { Router } from "express";
-import itemRouter from "./items";
-import basketRouter from "./basket";
+import { Router } from 'express';
+import itemRouter from './items';
+import basketRouter from './basket';
 
-const router = Router().use("/items", itemRouter).use("/basket", basketRouter);
+const router = Router().use('/items', itemRouter).use('/basket', basketRouter);
 
 export default router;
